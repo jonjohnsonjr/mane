a414bbb5b0bfab6118a02310d5c944c3675d0414
Jon Johnson <jon.johnson@chainguard.dev>
2023-06-13 12:52:57 -0700

    Launch otel-desktop-viewer by default

--- trace/trace.go
+++ trace/trace.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"errors"
 	"fmt"
+	"os"
+	"os/exec"
 
 	"go.opentelemetry.io/otel"
 	"go.opentelemetry.io/otel/exporters/otlp/otlptrace"
@@ -12,6 +14,19 @@ import (
 )
 
 func Main(ctx context.Context, main func(context.Context) error) error {
+	cmd := exec.CommandContext(ctx, "otel-desktop-viewer")
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	if err := cmd.Start(); err != nil {
+		return err
+	}
+
+	os.Setenv("OTEL_EXPORTER_OTLP_ENDPOINT", "http://localhost:4318")
+	os.Setenv("OTEL_TRACES_EXPORTER", "otlp")
+	os.Setenv("OTEL_EXPORTER_OTLP_PROTOCOL", "http/protobuf")
+
 	client := otlptracehttp.NewClient()
 	exporter, err := otlptrace.New(ctx, client)
 	if err != nil {
